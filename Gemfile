2ed4b6bf9ea25272d9ca435ca15b6af30295a105
Marian Vařecha <varecham@gmail.com>
2014-05-31 00:17:07 +0200

    removed unnecessary files

--- Gemfile
+++
@@ -1,9 +0,0 @@
-source "https://rubygems.org"
-
-gem 'berkshelf'
-gem 'foodcritic', '~> 3.0'
-
-group :integration do
-  gem 'test-kitchen'
-  gem 'kitchen-vagrant'
-end
--- test/integration/tarball/serverspec/tarball_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'serverspec'
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
-describe service('cassandra') do
-  it { should be_enabled }
-  it { should be_running }
-end
